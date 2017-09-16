eadb7e394e874d4da52c7ff5da63b056b94d95a1
cjvil <christiejvillanueva@gmail.com>
2017-09-15 22:00:13 -0700

    added redirect and ajax request on form submit

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,13 +1,17 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var requestHandler = require('./request-handler.js');
+var _ = require('underscore');
 
 exports.headers = {
-  'access-control-allow-origin': '*',
+  'Access-Control-Allow-Origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
+  'access-control-expose-headers': 'Location',
   'access-control-max-age': 10, // Seconds.
-  'Content-Type': 'text/html'
+  'Content-Type': 'text/html',
+  'Location': '/'
 };
 
 exports.serveAssets = function(res, asset, callback) {
@@ -17,21 +21,26 @@ exports.serveAssets = function(res, asset, callback) {
       res.end();
     } else {
       if (asset.endsWith('.css')) {
-        exports.headers['Content-Type'] = 'text/css';
+        callback(data, {'Content-Type': 'text/css'});
+      } else {
+        callback(data, exports.headers);
       }
-      callback(data);
     }
   });
 };
 
-exports.getURLs = function(res, asset, callback) {
+exports.getURLs = function(res, req, asset, callback) {
   fs.readFile(archive.paths.archivedSites + asset, function(err, data) {
     if (err) {
-      archive.addUrlToList(asset, () => {
-        archive.downloadUrls([asset], () => {
-          res.writeHead(404);
-          res.end();
-        });
+      archive.isUrlInList(asset.slice(1), (isInList) => {
+        if (!isInList) {
+          archive.addUrlToList(asset.slice(1), () => {
+            res.writeHead(404);
+            res.end();
+          });
+        } else {
+          requestHandler.methods.GET(req, res, '/loading.html');
+        }
       });
     } else {
       callback(data);
@@ -39,16 +48,33 @@ exports.getURLs = function(res, asset, callback) {
   });
 };
 
-// exports.archiveUrl = function(res, url, callback) {
-//   fs.readFile(__dirname + '/../archives/sites.txt', function(err, data) {
-//     if (err) {
-//       res.writeHead(404);
-//       res.end();
-//     } else {
-//       callback(data, url);
-//     }
-//   });
-// };
+exports.postURL = function(urlReq, req, res) {
+  if (urlReq.startsWith('www.')) {
+    archive.isUrlInList(urlReq, function(isInList) {
+      if (!isInList) {
+        archive.addUrlToList(urlReq, () => {
+          // var newHeader = _.extend({'Location': '/loading.html'}, exports.headers);
+          exports.headers['Location'] = '/loading.html';
+          res.writeHead(302, exports.headers);
+          res.end();
+        });
+      } else {
+        archive.isUrlArchived(urlReq, (archived) => {
+          if (archived) {
+            // var newHeader = _.extend({'Location': '/' + urlReq}, exports.headers);
+            exports.headers['Location'] = '/' + urlReq;
+            res.writeHead(302, exports.headers);
+            res.end();
+          } else {
+            exports.headers['Location'] = '/loading.html';
+            res.writeHead(302, exports.headers);
+            res.end();
+          }
+        });
+      }
+    });
+  }
+};
 
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -2,7 +2,6 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var helpers = require('./http-helpers.js');
 var url = require('url');
-var fs = require('fs');
 
 var methods = {
   GET: function(req, res, pathname) {
@@ -12,73 +11,35 @@ var methods = {
         res.end(data.toString());
       });
     } else if (pathname.startsWith('/www.')) {
-      helpers.getURLs(res, pathname, (data) => {
+      helpers.getURLs(res, req, pathname, (data) => {
         res.writeHead(200, helpers.headers);
         res.end(data.toString());
       });
     } else {
-      helpers.serveAssets(res, pathname, (data) => {
-        res.writeHead(200, helpers.headers);
+      helpers.serveAssets(res, pathname, (data, header) => {
+        res.writeHead(200, header);
         res.end(data.toString());
       });
     }
   },
   POST: function(req, res, pathname) {
-    // var writeUrlToFile = function(data, url) {
-    //   var content = JSON.parse(data);
-    //   content[url] = true;
-    //   fs.writeFile(__dirname + '/../archives/sites.txt', JSON.stringify(content), (err) => {
-    //     if (err) {
-    //       res.writeHead(404);
-    //       res.end();  
-    //     } else {
-    //       res.writeHead(302, helpers.headers);
-    //       res.end();
-    //     }
-    //   });
-    // };
-    
+    console.log(req.url);
     var urlReq = '';
     req.on('data', (chunk) => {
       urlReq += chunk;
     });
-    req.on('end', () => {
+    req.on('end', function() {
       urlReq = urlReq.slice(4);
-      // helpers.archiveUrl(res, urlReq, writeUrlToFile);
-      // fs.appendFile(archive.paths.list, urlReq + '\n', (err) => {
-      //   if (err || !urlReq.startsWith('www.')) {
-      //     res.writeHead(404);
-      //     res.end();  
-      //   } else {
-      //     res.writeHead(302, helpers.headers);
-      //     res.end();
-      //   }
-      // });
-      if (urlReq.startsWith('www.')) {
-        archive.isUrlInList(urlReq, function(isInList) {
-          if (!isInList) {
-            archive.addUrlToList(urlReq, () => {
-              res.writeHead(302, helpers.headers);
-              res.end();
-            });
-          } else {
-            archive.isUrlArchived(urlReq, (archived) => {
-              if (archived) {
-                methods.GET(req, res, '/' + urlReq);
-              } else {
-                methods.GET(req, res, '/loading.html');
-              }
-            });
-          }
-        });
-      }
-      
+      helpers.postURL(urlReq, req, res);
     });
-    
-    
-    
+  },
+  OPTIONS: function(req, res) {
+    res.writeHead(200, helpers.headers);
+    res.end();
   }
 };
+
+exports.methods = methods;
 
 
 exports.handleRequest = function (req, res) {
